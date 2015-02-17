18ea3b76f962d170606710df0d00f26f9982c2bb
Josh Clark <jpc8112@goclarks.com>
2015-02-17 09:33:37 -0600

    Ensure /etc/monit.d exists before adding template

--- layer-custom/recipes/esmonit.rb
+++ layer-custom/recipes/esmonit.rb
@@ -1,4 +1,12 @@
-template "/etc/monit.d/elasticsearch-monit.conf" do
+config_dir = "/etc/monit.d"
+
+directory config_dir do
+  owner node[:user][:username]
+  group node[:user][:username]
+  recursive true
+end
+
+template "#{config_dir}/elasticsearch-monit.conf" do
   source "elasticsearch.monitrc.conf.erb"
   mode 0440
   owner "root"
