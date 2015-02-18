a54ac56e9e303c417d3ca51b2f8450e7d4e0f59d
Josh Clark <jpc8112@goclarks.com>
2015-02-18 16:28:00 -0600

    include the actual file name

--- layer-custom/recipes/nginx-remove-default-site.rb
+++ layer-custom/recipes/nginx-remove-default-site.rb
@@ -1,4 +1,5 @@
-file "#{node.elasticsearch[:nginx][:dir]}/sites-enabled" do
+file "#{node.elasticsearch[:nginx][:dir]}/sites-enabled/default" do
   action :delete
+  force_unlink: true
   notifies :reload, 'service[nginx]'
 end
